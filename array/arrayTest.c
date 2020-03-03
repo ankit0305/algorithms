bf3923820b0bf21d862b6a7ad0a3157f3ece3680
ankit0305 <jaiswal.ankit2000@gmail.com>
2020-03-04 00:01:13 +0530

    array addition

---
+++ array/arrayTest.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+
+//For Integer
+int m[]={};
+int counterm=0;
+
+while(m[counterm]!='\0'){
+	counterm+=1;
+	
+}
+printf("%d\n",counterm);
+
+//For Character
+/*char n[]="Hello";
+int countern=0;
+
+while(n[countern]!='\0'){
+	countern+=1;
+	
+}
+printf("%d\n",countern);
+*/
+
+return 0;
+
+}
